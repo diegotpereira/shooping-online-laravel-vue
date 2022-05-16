a796e43ab1a9dda739014d54afe2dba9144c0fd7
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-05-16 19:14:51 -0300

    first commit

---
+++ resources/js/components/painel/Painel.vue
@@ -0,0 +1,91 @@
+<template>
+    <div>
+        <div class="row">
+            <div class="col-md-12">
+                <div>
+                    <h2>Olá, </h2>
+                    <hr>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3">
+                <a href="">
+                <div class="card">
+                    <div class="card-body">
+                        <template>
+                            <div>
+                                
+                            </div>
+                        </template>
+                        <template>
+                            <h2 class="text-center mb-3 text-success">Pedidos</h2>
+                            <h3 class="text-center text-success">
+                                <span class="badge badge-success"></span>
+                            </h3>
+                        </template>
+                    </div>
+                </div>
+                </a>
+            </div>
+            <div class="col-md-3">
+                <a href="">
+                <div class="card">
+                    <div class="card-body">
+                        <template>
+                            <div>
+                                
+                            </div>
+                        </template>
+                        <template>
+                            <h2 class="text-center mb-3 text-success">Clientes</h2>
+                            <h3 class="text-center text-success">
+                                <span class="badge badge-success"></span>
+                            </h3>
+                        </template>
+                    </div>
+                </div>
+                </a>
+            </div>
+            <div class="col-md-3">
+                <a href="">
+                <div class="card">
+                    <div class="card-body">
+                        <template>
+                            <div>
+                                
+                            </div>
+                        </template>
+                        <template>
+                            <h2 class="text-center mb-3 text-success">Inventário</h2>
+                            <h3 class="text-center text-success">
+                                <span class="badge badge-success"></span>
+                            </h3>
+                        </template>
+                    </div>
+                </div>
+                </a>
+            </div>
+            <div class="col md-3">
+                <div class="card">
+                    <div class="card-body">
+                        <template>
+                            <div>
+                                
+                            </div>
+                        </template>
+                        <template>
+                            <h2 class="text-center mb-3 text-success">Total de vendas semanais</h2>
+                            <h3 class="text-center text-success">
+                                <span class="badge badge-success"></span>
+                            </h3>
+                            <h3 class="text-center text-success">
+                                <span class="badge badge-success"></span>
+                            </h3>
+                        </template>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ resources/views/frontend/home.blade.php
@@ -0,0 +1,35 @@
+<div class="">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="carousel slide" data-ride="carousel">
+
+                <!-- indicadores -->
+                <ul class="carousel-indicators">
+                    <li data-target="#demo" data-slide-to="0" class="active"></li>
+                    <li data-target="#demo" data-slide-to="1"></li>
+                </ul>
+
+                <!-- exibe o slide -->
+                <div class="carousel-inner">
+                    <div class="carousel-item-active">
+                        <img src="" alt="">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="" alt="">
+                    </div>
+                </div>
+
+                <!-- controles direito e esquerdo -->
+                <a href="" class="carousel-control-prev">
+                    <span class="carousel-control-prev-icon"></span>
+                </a>
+                <a href="" class="carousel-control-next-icon">
+                    <span class="carousel-control-next-icon"></span>
+                </a>
+            </div>
+            <div>
+                <classmt-4 class="text-center">Seus Produtos</classmt-4>
+            </div>
+        </div>
+    </div>
+</div>
